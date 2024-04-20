c2c22320c5fe3ba43ad4bc2bb25deeb0f52fb972
unknown <raziyebayat75@gmail.com>
2024-04-20 16:56:18 +0330

    create form with grid

--- js/app.js
+++ js/app.js
@@ -3,7 +3,4 @@ import { schema1, schema2 } from "./schema.js";
 $(document).ready(function () {
   $("#myform").jsonForm(schema1);
   $("#myform2").jsonForm(schema2);
-  $("#myForm").on("render", function () {
-    $("select.dropdown").dropdown();
-  });
 });
--- js/schema.js
+++ js/schema.js
@@ -49,67 +49,73 @@ export const schema1 = {
 
 export const schema2 = {
   schema: {
-    part1: {
-      type: "object",
-      properties: {
-        Integer: {
-          type: "integer",
-          title: "Integer",
-          name: "integer",
-          default: 101,
-        },
-        Email: {
-          type: "email",
-          title: "Email",
-          name: "email",
-          default: "jack",
-        },
-      },
+    Integer: {
+      type: "integer",
+      title: "Integer",
+      name: "integer",
+      default: 101,
     },
-    part2: {
-      type: "object",
-      properties: {
-        Decimal: {
-          type: "string",
-          title: "Decimal",
-          name: "decimal",
-          default: "1.1.1",
-        },
-        Number: {
-          type: "string",
-          title: "Number",
-          name: "number",
-          default: "+200",
-        },
-      },
+    Email: {
+      type: "email",
+      title: "Email",
+      name: "email",
+      default: "jack",
     },
-    part3: {
-      type: "object",
-      properties: {
-        URL: {
-          type: "string",
-          title: "URL",
-          name: "url",
-          default: "ww.fakeurl.com",
-        },
-        RegEx: {
-          type: "string",
-          title: "RegEx",
-          name: "regex",
-          default: "joe",
-        },
-      },
+    Decimal: {
+      type: "string",
+      title: "Decimal",
+      name: "decimal",
+      default: "1.1.1",
+    },
+    Number: {
+      type: "string",
+      title: "Number",
+      name: "number",
+      default: "+200",
+    },
+    URL: {
+      type: "string",
+      title: "URL",
+      name: "url",
+      default: "ww.fakeurl.com",
+    },
+    RegEx: {
+      type: "string",
+      title: "RegEx",
+      name: "regex",
+      default: "joe",
     },
   },
   form: [
     {
-      key: "part1",
-    },
-    {
-      key: "part2",
-    },
-    {
-      key: "part3",
+      type: "fieldset",
+      htmlClass: "ui two column grid",
+      items: [
+        {
+          key: "Integer",
+          htmlClass: "eight wide column",
+        },
+        {
+          key: "Decimal",
+          htmlClass: "eight wide column",
+        },
+        {
+          key: "URL",
+          htmlClass: "eight wide column",
+        },
+        {
+          key: "Email",
+          htmlClass: "eight wide column",
+        },
+        {
+          key: "Number",
+          htmlClass: "eight wide column",
+        },
+        {
+          key: "RegEx",
+          htmlClass: "eight wide column",
+        },
+      ],
     },
     {
       type: "button",
@@ -121,7 +127,6 @@ export const schema2 = {
         $.each(formData, function () {
           formDataObject[this.name] = this.value;
         });
-        // alert(JSON.stringify(formDataObject, null, 2));
         console.log(formDataObject);
       },
     },
